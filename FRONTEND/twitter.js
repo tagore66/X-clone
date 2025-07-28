1cceead371a040d78e6d5fab041b5780d0505f61
tagore66 <tagoreatreyapurapu@gmail.com>
2025-07-28 20:03:36 +0530

    Update twitter.js

--- FRONTEND/twitter.js
+++ FRONTEND/twitter.js
@@ -36,7 +36,7 @@ signupSubmitBtn.onclick = async () => {
     return;
   }
   try {
-    const res = await fetch(`${backendURL}/auth/signup`, {
+    const res = await fetch(`https://x-clone-k842.onrender.com/auth/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, email, password }),
