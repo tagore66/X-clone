0e21bd66d33e480d5b29d216910a482274cdf1dd
tagore66 <tagoreatreyapurapu@gmail.com>
2025-07-28 21:24:27 +0530

    Update twitter.js

--- FRONTEND/twitter.js
+++ FRONTEND/twitter.js
@@ -1,4 +1,4 @@
-const backendURL = 'https://x-clone-k842.onrender.com';
+const backendURL = 'https://x-clone-1uw8.onrender.com';
 const messageDiv = document.getElementById('message');
 
 // Signup modal controls
